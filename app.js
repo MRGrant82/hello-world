82fa6dfdb1b27c4f5f8a836f561763d6905a74b8
Michael Grant <serus12345@gmail.com>
2023-02-22 19:29:53 -0500

    Added javascript

---
+++ app.js
@@ -0,0 +1,19 @@
+
+    let myName = "Michael";
+    alert("Welcome to my website!" + "  I am " + myName + ".");
+    
+    let userName = prompt("Enter your name please");
+    alert("Welcome " + userName + " !")
+
+    document.write("Welcome, " + "    " + userName)
+
+    let coffeeChoice = prompt("Do you like coffee? Type yes or no");
+    
+    if (coffeeChoice == 'yes')
+    {
+        document.write("     Ahh!  You're also an enjoyer of the delicious black soup! I know this because you said " + coffeeChoice + ".")
+    }
+    else
+    {document.write("We will work on your beverage choices.")}  
+
+    
